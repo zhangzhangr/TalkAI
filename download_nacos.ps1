18ef66f5fcfb46692ec697a4765d5287829cf164
zhangzhangr <554150332@qq.com>
2026-05-27 18:29:08 +0800

    chore: 清理无用文件 - 移除一次性脚本/测试图片/存根模块/编译产物/日志

--- download_nacos.ps1
+++
@@ -1,6 +0,0 @@
-$url = "https://github.com/alibaba/nacos/releases/download/2.3.2/nacos-server-2.3.2.zip"
-$out = "D:\tool\nacos.zip"
-Write-Host "Downloading Nacos 2.3.2..."
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $url -OutFile $out -ErrorAction Stop
-Write-Host "Download complete"
